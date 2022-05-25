1371a10c774a7718350b7a55fa05c0a95c645430
Анастасия Пакаева <staroxy63@gmail.com>
2022-05-25 20:46:45 +0300

    reformate

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -2,7 +2,7 @@ public class CreditPaymentService {
     public double calculate(double sum, int time, double percent) {
 
         double stake = (percent / 100) / 12; //ставка в месяц
-        double annuity = stake * Math.pow((1 + stake), time) / (Math.pow((1+stake), time) - 1); //коэффициент аннуитета
+        double annuity = stake * Math.pow((1 + stake), time) / (Math.pow((1 + stake), time) - 1); //коэффициент аннуитета
         double payment = annuity * sum;
 
         return payment;
